feab39ea52877224373dd6821c600f381bdf2103
Matea Cotic <cotic.matea@gmail.com>
2020-07-08 00:00:09 +0200

    css

--- view/_header.php
+++ view/_header.php
@@ -5,7 +5,22 @@
     <title>teka</title>
 
     <style>
-        body { font-family: "Trebuschet MS", Helvetica, sans-serif; }
+        body 
+        { 
+            font-family: "Trebuschet MS", Helvetica, sans-serif; 
+            background-color: #C9CBA3;
+            color: #723D46;
+        }
+        input { background-color:#FFE1A8; font-size: 30px; }
+        input:hover 
+        { 
+            background-color:#E26D5C;
+            color: #472D30; 
+        }
+        ul { list-style: none; }
+        button { background-color:#FFE1A8; font-size: 18px; }
+        button:hover { background-color:#E26D5C; color: #472D30; }
+
     </style>
 </head>
 <body>
